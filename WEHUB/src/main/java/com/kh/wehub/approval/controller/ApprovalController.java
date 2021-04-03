9e5f6a39c87fc8d793a2815fc8ef2c464507a292
M-Zl <76435884+M-Zl@users.noreply.github.com>
2021-04-03 20:33:26 +0900

    왜 안올라가

--- WEHUB/src/main/java/com/kh/wehub/approval/controller/ApprovalController.java
+++ WEHUB/src/main/java/com/kh/wehub/approval/controller/ApprovalController.java
@@ -1,5 +1,30 @@
 package com.kh.wehub.approval.controller;
 
-public class ApprovalController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.wehub.approval.model.service.ApprovalService;
+import com.kh.wehub.member.model.service.MemberService;
+import com.kh.wehub.member.model.vo.Member;
+
+@Controller
+@RequestMapping("/approval")
+@SessionAttributes("loginMember")
+public class ApprovalController {
+   /* @Autowired */
+   private ApprovalService service;
+   
+   @Autowired
+   private MemberService service2;
+
+   @RequestMapping(value = "/approval", method = { RequestMethod.GET })
+   public String approvalView(@SessionAttribute(name = "loginMember", required = false) Member loginMember) {
+      
+      return "/approval/approval";
+   }
 }
